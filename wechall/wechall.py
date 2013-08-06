378805a31480cdca615488a59512e0de55077d95
Maverick JS <mavjs01@gmail.com>
2013-08-07 02:19:40 +0800

    wechall proper

--- wechall/wechall.py
+++ wechall/wechall.py
@@ -2,28 +2,22 @@
 
 import requests
 
-class wechall(object):
-    """
-    A class to parse scores out of wechall
-    """
-    def __init__(self, username):
-        self.username = username
-        self.baseurl = 'http://www.wechall.net/wechall.php?username=!sites '
-        self.challs = []
+baseurl = 'http://www.wechall.net/wechall.php?username=!sites '
+challs = []
+challdic = {}
 
-    def getall(self):
-        req = requests.get(self.baseurl+self.username)
-        resp = req.content.split(' ')
-        index = int(resp.index('primary:'))+1
-        allchalls = resp[index:]
-        for i in range(0, len(allchalls)):
-            self.challs.append(allchalls[i][:-1])
-        return self.challs
+def getall(username):
+    req = requests.get(baseurl+username)
+    resp = req.content.split(' ')
+    index = int(resp.index('primary:'))+1
+    allchalls = resp[index:]
+    for i in range(0, len(allchalls)):
+        challs.append(allchalls[i][:-1])
+    return challs
 
-    def challper(self):
-        challdic = {}
-        for i in self.challs:
-            perc = i.split('(')[1].split(')')[0]
-            name = i.split('(')[0]
-            challdic[name] = perc
-        return challdic
+def challper(self):
+    for i in challs:
+        perc = i.split('(')[1].split(')')[0]
+        name = i.split('(')[0]
+        challdic[name] = perc
+    return challdic
